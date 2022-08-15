7f033c32671c3a4a72ad07ff07d615d742cc7f47
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-08-15 13:07:35 -0700

    Adds basic styles to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,22 @@
+import "./Landing.css"
+
 const Landing = () => {
   return(
-    <div>
-      <h1>Landing</h1>
+    <div className="landing-wrapper">
+      <div className="landing-bubble">
+        <h1>Your Neighborhood</h1>
+        <h1>Catsitting App</h1>
+      </div>
+      <div className="landing-features">
+        <div className="landing-request-catsitter">
+          <h1>Request a Catsitter</h1>
+          <p className="p-landing">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div className="landing-book-catsitting">
+          <h1>Book a Catsitting</h1>
+          <p className="p-landing">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </div>  
     </div>
   );
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,12 @@
 const SignUp = () => {
   return(
-    <div>
-      <h1>Sign Up</h1>
+    <div className="signup-wrapper">
+      <div className="signup-header">
+        <h1>Sign Up</h1>
+        <p className="p-signup">Create an account with us to view your neighbor's catsitting requests and request catsitting from your neighbors</p>
+      </div>
+      <div className="signup-form">
+      </div>
     </div>
   );
 };
