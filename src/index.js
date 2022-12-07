ecfbaeb5c9458de812e0135558db2cf05c716d6a
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-12-06 18:49:52 -0800

    Makes more changes for deployment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+require('dotenv').config()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
