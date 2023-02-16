fe46a184f195abb8c36e4877564d95c7c0fe76d3
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2023-02-16 13:39:46 -0800

    Update for deploy

--- src/components/AdditionalNotes.jsx
+++
@@ -1,9 +0,0 @@
-const AdditionalNotes = (props) => {
-  return(
-    <ul>
-      <li className="p-pills">- {props.additionalnote.additionalnote}</li>
-    </ul>
-  )
-}
-
-export default AdditionalNotes;
--- src/components/Medications.jsx
+++
@@ -1,9 +0,0 @@
-const Medications = (props) => {
-  return(
-    <ul>
-      <li className="p-pills">- {props.medication.medication}</li>
-    </ul>
-  )
-}
-
-export default Medications
